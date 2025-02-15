84ec483885a59eba77f5eeb6321ec6a72715d3f2
aqvPC <anhquanvo911@gmail.com>
2025-02-15 12:05:28 +0700

    MVC

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -3,7 +3,7 @@ import express from "express"
 const config = (app) => {
     app.use(express.static('./src/public'));
     app.set("view engine", "ejs");
-    app.set("views,", "./src/views");
+    app.set("views", "./src/views");
 }
 
 export default config;
